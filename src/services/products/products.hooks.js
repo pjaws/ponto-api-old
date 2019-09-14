42c5e632254a30552a4afe34484819f8364b7dd2
Paul Jaworski <paulmattjaws@gmail.com>
2019-09-14 14:25:11 -0700

    populate variants, images, options on get/find products

--- src/services/products/products.hooks.js
+++ src/services/products/products.hooks.js
@@ -1,24 +1,58 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
+const { populate } = require('feathers-hooks-common');
+
+const productVariantsSchema = {
+  include: {
+    service: 'product_variants',
+    nameAs: 'variants',
+    parentField: 'productId',
+    childField: 'id',
+  },
+};
+const productImagesSchema = {
+  include: {
+    service: 'product_images',
+    nameAs: 'images',
+    parentField: 'productId',
+    childField: 'id',
+  },
+};
+const productOptionsSchema = {
+  include: {
+    service: 'product_options',
+    nameAs: 'options',
+    parentField: 'productId',
+    childField: 'id',
+  },
+};
 
 module.exports = {
   before: {
-    all: [ authenticate('jwt') ],
+    all: [authenticate('jwt')],
     find: [],
     get: [],
     create: [],
     update: [],
     patch: [],
-    remove: []
+    remove: [],
   },
 
   after: {
     all: [],
-    find: [],
-    get: [],
+    find: [
+      populate({ schema: productVariantsSchema }),
+      populate({ schema: productImagesSchema }),
+      populate({ schema: productOptionsSchema }),
+    ],
+    get: [
+      populate({ schema: productVariantsSchema }),
+      populate({ schema: productImagesSchema }),
+      populate({ schema: productOptionsSchema }),
+    ],
     create: [],
     update: [],
     patch: [],
-    remove: []
+    remove: [],
   },
 
   error: {
@@ -28,6 +62,6 @@ module.exports = {
     create: [],
     update: [],
     patch: [],
-    remove: []
-  }
+    remove: [],
+  },
 };
